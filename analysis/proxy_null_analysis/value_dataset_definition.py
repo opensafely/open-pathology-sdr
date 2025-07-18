1fc1dc078e3cb18eacae228a310cb5c75fb8f973
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2025-07-18 13:15:27 +0000

    Update histogram measures definition to include lower and upper bounds

--- analysis/proxy_null_analysis/value_dataset_definition.py
+++ analysis/proxy_null_analysis/value_dataset_definition.py
@@ -1,11 +1,12 @@
-# This script extracts numeric values for testing if there are proxy null values
+# This script extracts numeric, upper and lower bound values for testing if there are proxy null values
+# Args: --codelist [codelist]
 
 import argparse
 from ehrql import codelist_from_csv
 from ehrql.tables.core import patients
 from ehrql.tables.tpp import (
     practice_registrations as registrations,
-    clinical_events
+    clinical_events_ranges
 )
 from ehrql import create_dataset
 
@@ -21,14 +22,17 @@ end_date = "2024-01-01"
 
 # Codelists
 codelist = codelist_from_csv(args.codelist, column="code")
-codelist_events = clinical_events.where(
-    clinical_events.snomedct_code.is_in(codelist)
-    & clinical_events.date.is_on_or_between(start_date, end_date)
+
+codelist_events = clinical_events_ranges.where(
+    clinical_events_ranges.snomedct_code.is_in(codelist)
+    & clinical_events_ranges.date.is_on_or_between(start_date, end_date)
 )
 
 # Extract numeric values for codelist
 dataset.codelist_event_count = codelist_events.count_for_patient()
 dataset.numeric_value = codelist_events.sort_by(codelist_events.date).first_for_patient().numeric_value
+dataset.upper_bound = codelist_events.sort_by(codelist_events.date).first_for_patient().upper_bound
+dataset.lower_bound = codelist_events.sort_by(codelist_events.date).first_for_patient().lower_bound
 
 dataset.configure_dummy_data(population_size=1000)
 dataset.define_population(patients.exists_for_patient())
