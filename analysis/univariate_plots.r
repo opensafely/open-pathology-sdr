d796731e715e34fcf9550b65f4a6d0d7900fad3b
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2025-10-14 15:07:05 +0000

    Fixing plots

--- analysis/univariate_plots.r
+++ analysis/univariate_plots.r
@@ -1,53 +1,53 @@
 # USAGE: r analysis/univariate_plots.r
 
-# install.packages('sf')
-# library(sf)
+install.packages('sf')
+library(sf)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-# df <- read.csv("output/output/OP SDR results table - Sheet5.csv")
+df <- read.csv("output/output/OP SDR results table - Sheet5.csv")
 
-# # Load shapefile
-# uk_regions <- st_read("output/output/NUTS_Level_1_January_2018_GCB_in_the_United_Kingdom_2022_-2753267915301604886.geojson")
-# uk_regions$nuts118nm <- gsub(" \\(England\\)", "", uk_regions$nuts118nm)
-# uk_regions$nuts118nm <- gsub(" of England", "", uk_regions$nuts118nm)
+# Load shapefile
+uk_regions <- st_read("output/output/NUTS_Level_1_January_2018_GCB_in_the_United_Kingdom_2022_-2753267915301604886.geojson")
+uk_regions$nuts118nm <- gsub(" \\(England\\)", "", uk_regions$nuts118nm)
+uk_regions$nuts118nm <- gsub(" of England", "", uk_regions$nuts118nm)
 
-# # Pivot data (keep Field.name!)
-# df_long <- df %>%
-#   pivot_longer(
-#     cols = -(c(Field.name, Region)), 
-#     names_to = "Test", 
-#     values_to = "Value"
-#   )
+# Pivot data (keep Field.name!)
+df_long <- df %>%
+  pivot_longer(
+    cols = -(c(Field.name, Region)), 
+    names_to = "Test", 
+    values_to = "Value"
+  )
 
-# # Merge shapefile and data
-# merged <- uk_regions %>%
-#   left_join(df_long, by = c("nuts118nm" = "Region")) %>%
-#   filter(!is.na(Test), !is.na(Field.name))
+# Merge shapefile and data
+merged <- uk_regions %>%
+  left_join(df_long, by = c("nuts118nm" = "Region")) %>%
+  filter(!is.na(Test), !is.na(Field.name))
 
-# # Reorder field names
-# merged$Field.name <- factor(
-#   merged$Field.name,
-#   levels = c("Test Value", "Upper Bound", "Lower Bound", "Equality Comparator", "Differential Comparator")
-# )
+# Reorder field names
+merged$Field.name <- factor(
+  merged$Field.name,
+  levels = c("Test Value", "Upper Bound", "Lower Bound", "Equality Comparator", "Differential Comparator")
+)
 
-# # Facet by BOTH Test and Field.name
-# p <- ggplot(merged) +
-#   geom_sf(aes(fill = Value), color = "white") +
-#   scale_fill_gradient(
-#     low = "white",
-#     high = "red",
-#     name = "Non-null %"
-#   ) +
-#   facet_grid(Field.name ~ Test, switch = 'y') +
-#   theme(
-#     axis.text.x = element_blank(),
-#     axis.text.y = element_blank(),
-#     axis.ticks = element_blank(),
-#     text = element_text(size = 15))
+# Facet by BOTH Test and Field.name
+p <- ggplot(merged) +
+  geom_sf(aes(fill = Value), color = "white") +
+  scale_fill_gradient(
+    low = "white",
+    high = "red",
+    name = "Non-null %"
+  ) +
+  facet_grid(Field.name ~ Test, switch = 'y') +
+  theme(
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    text = element_text(size = 16))
 
-# ggsave("output/output/regions_plot_grid.png", plot = p, width = 10, height = 10, dpi = 300)
+ggsave("output/output/regions_plot_grid.png", plot = p, width = 10, height = 10, dpi = 300)
 
 # ------------- Time plot ----------------------------
 
